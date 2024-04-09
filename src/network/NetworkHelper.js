e20d7dde2b02ea1634080c1ca5718b81fcf39b32
Christian Schmitz <cschmitz398@gmail.com>
2024-04-09 12:27:16 -0600

    context safe NetworkHelper.getUtxo and getUtxos

--- src/network/NetworkHelper.js
+++ src/network/NetworkHelper.js
@@ -1,5 +1,12 @@
-import { Address, TxInput, Value } from "@helios-lang/ledger"
+import {
+    Address,
+    TxInput,
+    TxOutput,
+    TxOutputId,
+    Value
+} from "@helios-lang/ledger"
 import { selectSmallestFirst } from "../coinselection/index.js"
+import { None } from "@helios-lang/type-utils"
 
 /**
  * @typedef {import("../coinselection/index.js").CoinSelection} CoinSelection
@@ -18,6 +25,47 @@ export class NetworkHelper {
      */
     constructor(network) {
         this.network = network
+    }
+
+    /**
+     * @template [CSpending=unknown]
+     * @template [CStaking=unknown]
+     * @param {TxOutputId} id
+     * @param {Option<Address<CSpending, CStaking>>} address
+     * @returns {Promise<TxInput<CSpending, CStaking>>}
+     */
+    async getUtxo(id, address = None) {
+        const utxo = await this.network.getUtxo(id)
+
+        if (address) {
+            if (!utxo.address.isEqual(address)) {
+                throw new Error(
+                    `expected Address UTxO ${id.toString()} to be ${address.toString()}, got address ${utxo.address.toString()}`
+                )
+            }
+
+            return new TxInput(
+                id,
+                new TxOutput(
+                    address,
+                    utxo.value,
+                    utxo.datum,
+                    utxo.output.refScript
+                )
+            )
+        } else {
+            return utxo
+        }
+    }
+
+    /**
+     * @template CSpending
+     * @template CStaking
+     * @param {Address<CSpending, CStaking>} address
+     * @returns {Promise<TxInput<CSpending, CStaking>[]>}
+     */
+    async getUtxos(address) {
+        return this.network.getUtxos(address)
     }
 
     /**
