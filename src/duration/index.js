20f290b69bd97a1c95346fe70c126cc25e749399
Christian Schmitz <cschmitz398@gmail.com>
2024-04-11 16:36:12 -0600

    typical tx validity timerange offsets

--- src/duration/index.js
+++ src/duration/index.js
@@ -4,3 +4,8 @@ export const MINUTE = 60 * SECOND
 export const HOUR = 60 * MINUTE
 export const DAY = 24 * HOUR
 export const WEEK = 7 * DAY
+
+/**
+ * From experience: -90 seconds and +300 seconds are good offsets to wrt. `now()` to set the validity time range to
+ */
+export const DEFAULT_TX_VALIDITY_OFFSETS = [-90 * SECOND, 300 * SECOND]
