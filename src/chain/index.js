c376fb0d7d4ca077e8ba136cb5416f6da07f7bbe
Christian Schmitz <cschmitz398@gmail.com>
2024-05-03 19:10:11 -0600

    exported TxSummaryJsonSafe

--- src/chain/index.js
+++ src/chain/index.js
@@ -1,3 +1,7 @@
 export { TxChain } from "./TxChain.js"
 export { TxChainBuilder } from "./TxChainBuilder.js"
 export { TxSummary } from "./TxSummary.js"
+
+/**
+ * @typedef {import("./TxSummary.js").TxSummaryJsonSafe} TxSummaryJsonSafe
+ */
