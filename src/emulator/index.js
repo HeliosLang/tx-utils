b636617f286b6cb4821e11882a9ef5f78a3a8baf
Christian Schmitz <cschmitz398@gmail.com>
2024-08-27 16:13:00 -0600

    exported EmulatorTx, GenesisTx and RegularTx

--- src/emulator/index.js
+++ src/emulator/index.js
@@ -1,1 +1,7 @@
 export { Emulator } from "./Emulator.js"
+export { GenesisTx } from "./GenesisTx.js"
+export { RegularTx } from "./RegularTx.js"
+
+/**
+ * @typedef {import("./EmulatorTx.js").EmulatorTx} EmulatorTx
+ */
