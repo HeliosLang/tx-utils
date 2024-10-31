9a6c60ed0a47fb4c7178ba7c8fd82f275f271e59
Christian Schmitz <cschmitz398@gmail.com>
2024-10-31 13:09:31 -0600

    - ledger lib upgraded - CachedRefScriptRegistry make a copy of the utxo and attaches the original program

--- src/txbuilder/CachedRefScriptRegistry.js
+++ src/txbuilder/CachedRefScriptRegistry.js
@@ -1,6 +1,5 @@
 import { bytesToHex } from "@helios-lang/codec-utils"
-import { TxOutputId } from "@helios-lang/ledger"
-import { TxInput } from "@helios-lang/ledger"
+import { TxInput, TxOutput, TxOutputId } from "@helios-lang/ledger"
 import { None } from "@helios-lang/type-utils"
 
 /**
@@ -83,7 +82,12 @@ class CachedRefScriptRegistry {
 
             const input = await this.network.getUtxo(inputId)
 
-            return { program, input }
+            // make a copy of the input with the full original program, so we are sure to get all the additional information (source mapping etc.)
+            const inputCopy = new TxInput(
+                inputId,
+                new TxOutput(input.address, input.value, input.datum, program)
+            )
+            return { program, input: inputCopy }
         } else {
             return None
         }
